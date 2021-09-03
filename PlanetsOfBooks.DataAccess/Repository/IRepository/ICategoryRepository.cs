7ac1165b63a512452191cc48a730aa6813aff0a1
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2021-09-03 19:45:04 +0200

    Category Repository and Interface

---
+++ PlanetsOfBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using PlanetOfBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetOfBooks.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
