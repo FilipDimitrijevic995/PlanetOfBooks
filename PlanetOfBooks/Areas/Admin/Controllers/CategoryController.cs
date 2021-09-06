a9eeabc71a16d37feffb272a8479ae86a7ac4739
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2021-09-06 22:05:08 +0200

    Working on Category CRUD

---
+++ PlanetOfBooks/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlanetOfBooks.DataAccess.Repository.IRepository;
+using PlanetOfBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanetOfBooks.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if (id == null) 
+            {
+                // this is for create Category
+                return View(category);
+            }
+            // this is for edit
+            category = _unitOfWork.Category.GetFirstOrDefault();
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+
+        #endregion
+    }
+}
