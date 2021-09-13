d5ccde2215c729f0b132b0b94b8cda555a7097f8
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2021-09-13 20:15:23 +0200

    Added new Fields to Registation

---
+++ PlanetOfBooks/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PlanetOfBooks.DataAccess.Data;
+
+[assembly: HostingStartup(typeof(PlanetOfBooks.Areas.Identity.IdentityHostingStartup))]
+namespace PlanetOfBooks.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
