e67b7dc49231429bcdd5abe92c5d889b71aabea9
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2021-09-13 13:03:32 +0200

    Added Company JS, Controller and Repository

--- PlanetsOfBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ PlanetsOfBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,10 +9,9 @@ namespace PlanetOfBooks.DataAccess.Repository.IRepository
     public interface IUnitOfWork : IDisposable
     {
         ICategoryRepository Category { get; }
-
+        ICompanyRepository Company { get; }
         ICoverTypeRepository CoverType { get; }
         IProductRepository Product { get; }
-
         ISP_Call SP_Call { get; }
 
         void Save();
--- PlanetsOfBooks.DataAccess/Repository/UnitOfWork.cs
+++ PlanetsOfBooks.DataAccess/Repository/UnitOfWork.cs
@@ -16,12 +16,14 @@ namespace PlanetOfBooks.DataAccess.Repository
         {
             _db = db;
             Category = new CategoryRepository(_db);
+            Company = new CompanyRepository(_db);
             CoverType = new CoverTypeRepository(_db);
             Product = new ProductRepository(_db);
             SP_Call = new SP_Call(_db);
         }
 
         public ICategoryRepository Category { get; private set; }
+        public ICompanyRepository Company { get; private set; }
         public ICoverTypeRepository CoverType { get; private set; }
         public IProductRepository Product { get; private set; }
         public ISP_Call SP_Call { get; private set; }
