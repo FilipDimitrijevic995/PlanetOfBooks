17db4a4378b582cd9efb00b5c4fe5ef0f7460137
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2021-09-19 13:35:24 +0200

    Adding Product to Shopping Cart

--- PlanetOfBooks/Areas/Customer/Controllers/HomeController.cs
+++ PlanetOfBooks/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PlanetOfBooks.DataAccess.Repository.IRepository;
 using PlanetOfBooks.Models;
@@ -7,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace PlanetOfBooks.Areas.Customer.Controllers
@@ -27,6 +29,42 @@ namespace PlanetOfBooks.Areas.Customer.Controllers
         {
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
             return View(productList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public IActionResult Details(ShoppingCart CartObject)
+        {
+            CartObject.Id = 0;
+            if (ModelState.IsValid)
+            {
+                // then we will add to cart
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                CartObject.ApplicationUserId = claim.Value;
+
+                ShoppingCart carFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(
+                        u => u.ApplicationUserId == CartObject.ApplicationUserId && u.ProductId == CartObject.ProductId, includeProperties: "Product"
+                    );
+
+                if (carFromDb == null)
+                { 
+                    // no records exists in database for that product for that user
+
+                }
+            }
+            else
+            {
+                var productFromDb = _unitOfWork.Product.
+                        GetFirstOrDefault(u => u.Id == CartObject.ProductId, includeProperties: "Category,CoverType");
+                ShoppingCart cartObj = new ShoppingCart()
+                {
+                    Product = productFromDb,
+                    ProductId = productFromDb.Id
+                };
+                return View(cartObj);
+            }
         }
 
         public IActionResult Details(int id)
