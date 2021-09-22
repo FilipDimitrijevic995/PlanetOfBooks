5f7d8414bbdffc17323be915922b381b0c4157bd
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2021-09-22 11:16:00 +0200

    Shopping Cart Increment Item

--- PlanetOfBooks/Areas/Customer/Controllers/CartController.cs
+++ PlanetOfBooks/Areas/Customer/Controllers/CartController.cs
@@ -56,5 +56,15 @@ namespace PlanetOfBooks.Areas.Customer.Controllers
 
             return View(ShoppingCartVM);
         }
+
+        public IActionResult Plus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId, includeProperties: "Product");
+            cart.Count += 1;
+            cart.Price = SD.GetPriceBasedOnQuantity(cart.Count, cart.Product.Price,
+                                                    cart.Product.Price50, cart.Product.Price100);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
