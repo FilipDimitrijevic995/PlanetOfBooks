e3e6f10518d8ac9b3ef8ddd3c40fa30f2c5d11ce
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2021-09-22 11:24:37 +0200

    Minus and Remove in Shopping Cart

--- PlanetOfBooks/Areas/Customer/Controllers/CartController.cs
+++ PlanetOfBooks/Areas/Customer/Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using PlanetOfBooks.DataAccess.Repository.IRepository;
@@ -64,6 +65,43 @@ namespace PlanetOfBooks.Areas.Customer.Controllers
             cart.Price = SD.GetPriceBasedOnQuantity(cart.Count, cart.Product.Price,
                                                     cart.Product.Price50, cart.Product.Price100);
             _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId, includeProperties: "Product");
+
+            if (cart.Count == 1)
+            {
+                var cnt = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count();
+                _unitOfWork.ShoppingCart.Remove(cart);
+                _unitOfWork.Save();
+                HttpContext.Session.SetInt32(SD.ssShoppingCart, cnt - 1);
+            }
+            else
+            {
+                cart.Count -= 1;
+                cart.Price = SD.GetPriceBasedOnQuantity(cart.Count, cart.Product.Price,
+                                                        cart.Product.Price50, cart.Product.Price100);
+                _unitOfWork.Save();
+
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId, includeProperties: "Product");
+
+
+            var cnt = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count();
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            HttpContext.Session.SetInt32(SD.ssShoppingCart, cnt - 1);
+
+
             return RedirectToAction(nameof(Index));
         }
     }
